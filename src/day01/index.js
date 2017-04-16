c9e7bfc767ea73a78b32c4dda9e5ac14da5379ca
Sergio Cinos <scinos@atlassian.com>
2017-04-16 13:30:13 +1000

    Eslint

--- src/day01/index.js
+++ src/day01/index.js
@@ -1,69 +1,70 @@
-const flipSign = (sign) => sign==="+"?"-":"+";
-const flipDirection = (direction) => direction === "Y" ? "X": "Y";
+const flipSign = sign => (sign === '+' ? '-' : '+');
+const flipDirection = direction => (direction === 'Y' ? 'X' : 'Y');
 
-const inc = function*(from, to) {
-    for (let i = from; i<=to; i++) yield i;
-}
+const inc = function* (from, to) {
+    for (let i = from; i <= to; i++) yield i;
+};
 
-const dec = function*(from, to) {
-    for (let i = from; i>=to; i--) yield i;
-}
+const dec = function* (from, to) {
+    for (let i = from; i >= to; i--) yield i;
+};
 
-const computePosition = (input) => input
+const computePosition = input => input
     // Split & sanitize
     .split(',').map(item => item.trim())
 
     // Get components (direction and steps)
     .map(item => item.match(/^([LR])([0-9]+)$/))
-    .map(match => ({turn: match[1], steps: Number(match[2])}))
+    .map(match => ({ turn: match[1], steps: Number(match[2]) }))
 
     // Compute the final steps
-    .reduce( ({places, position, direction, sign, firstVisitedTwice}, {turn, steps}) => {
+    .reduce(({ places, position, direction, sign, firstVisitedTwice }, { turn, steps }) => {
         const updatePosition = (coord, increment) => {
-            position[coord] = position[coord] + increment;
+            position[coord] += increment;
             places[position] = (places[position] || 0) + 1;
-            if (places[position] === 2 && !firstVisitedTwice) firstVisitedTwice = Object.assign({},position);
-        }
+            if (places[position] === 2 && !firstVisitedTwice) firstVisitedTwice = Object.assign({}, position);
+        };
 
         // Flip the sign
         if (
-            (direction === "Y" && turn === "L") ||
-            (direction === "X" && turn === "R")
+            (direction === 'Y' && turn === 'L') ||
+            (direction === 'X' && turn === 'R')
         ) sign = flipSign(sign);
 
         // Flip the direction
         direction = flipDirection(direction);
 
         // Compute which coordinate needs update based on the direction of the movement
-        const coordToUpdate = direction === "X" ? 0 : 1;
+        const coordToUpdate = direction === 'X' ? 0 : 1;
 
         // Compute how we should increment or decrement the coordinate based on the sign of movement
-        const counter = sign === "+" ? inc(1, steps) : dec(-1, -steps);
+        const counter = sign === '+' ? inc(1, steps) : dec(-1, -steps);
 
         // Compute how much we should change the coordinate based on the sign of movement
-        const step = sign === "+" ? 1 : -1;
+        const step = sign === '+' ? 1 : -1;
 
         // Update the position
-        for (let i of counter) updatePosition(coordToUpdate, step);
+        for (const _ of counter) updatePosition(coordToUpdate, step); // eslint-disable-line no-unused-vars
 
-        return {places, position, direction, sign, firstVisitedTwice};
+        return { places, position, direction, sign, firstVisitedTwice };
     }, {
         places: {
-            "0,0": 1
+            '0,0': 1,
         },
-        position: [0,0],
-        direction: "Y",
-        sign: "+",
-        firstVisitedTwice: null
+        position: [0, 0],
+        direction: 'Y',
+        sign: '+',
+        firstVisitedTwice: null,
     });
 
 const part1 = (input) => {
     const results = computePosition(input);
     return Math.abs(results.position[0]) + Math.abs(results.position[1]);
-}
+};
 
 const part2 = (input) => {
     const results = computePosition(input);
     return Math.abs(results.firstVisitedTwice[0]) + Math.abs(results.firstVisitedTwice[1]);
-}
-module.exports = {part1, part2};
+};
+
+module.exports = { part1, part2 };
--- test/day01/index.js
+++ test/day01/index.js
@@ -1,5 +1,5 @@
 const day1 = require('../../src/day01');
-const expect= require('chai').expect;
+const expect = require('chai').expect;
 const fs = require('fs');
 const path = require('path');
 
@@ -7,11 +7,11 @@ const part1 = day1.part1;
 const part2 = day1.part2;
 
 describe('--- Day 1: No Time for a Taxicab ---', () => {
-    describe ('--- Part 1 ---', () => {
-        it ('Following R2, L3 leaves you 2 blocks East and 3 blocks North, or 5 blocks away.', () => {
+    describe('--- Part 1 ---', () => {
+        it('Following R2, L3 leaves you 2 blocks East and 3 blocks North, or 5 blocks away.', () => {
             expect(part1('R2, L3')).to.equal(5);
         });
-        it ('R2, R2, R2 leaves you 2 blocks due South of your starting position, which is 2 blocks away.', () => {
+        it('R2, R2, R2 leaves you 2 blocks due South of your starting position, which is 2 blocks away.', () => {
             expect(part1('R2, R2, R2')).to.equal(2);
         });
         it('R5, L5, R5, R3 leaves you 12 blocks away.', () => {
@@ -20,17 +20,17 @@ describe('--- Day 1: No Time for a Taxicab ---', () => {
 
         it('Solves the input', () => {
             const input = fs.readFileSync(path.resolve(__dirname, './input.txt'), 'utf8');
-            expect(part1(input)).to.equal(181)
-        })
+            expect(part1(input)).to.equal(181);
+        });
     });
 
-    describe ('--- Part 2 ---', () => {
-        it ('If your instructions are R8, R4, R4, R8, the first location you visit twice is 4 blocks away, due East.', () => {
+    describe('--- Part 2 ---', () => {
+        it('If your instructions are R8, R4, R4, R8, the first location you visit twice is 4 blocks away', () => {
             expect(part2('R8, R4, R4, R8')).to.equal(4);
         });
         it('Solves the input', () => {
             const input = fs.readFileSync(path.resolve(__dirname, './input.txt'), 'utf8');
-            expect(part2(input)).to.equal(140)
-        })
-    })
+            expect(part2(input)).to.equal(140);
+        });
+    });
 });
